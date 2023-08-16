3ddf17e145b4ed6f17977103febbf393e3f8b011
Luan Teixeira <luanlts@outlook.com>
2023-08-16 10:47:32 -0300

    feat(etapa08): finaliza desafio pagina de favoritos
    finaliza o desafio proposto na etapa 08, criacao da pagina de favoritos
    utilizando listView e ListTile
    utilizando o metodo map da lista de favoritos para fazer a renderizacao
    de todas as palavras salvas como favoritas

--- lib/main.dart
+++ lib/main.dart
@@ -43,6 +43,11 @@ class MyAppState extends ChangeNotifier {
     }
     notifyListeners();
   }
+
+  void removeFavorite(pair) {
+    favorites.remove(pair);
+    notifyListeners();
+  }
 }
 
 class MyHomePage extends StatefulWidget {
@@ -139,17 +144,23 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var favorites = appState.favorites;
-
-    return Center(
-      child: ListView(
-        children: [
-          ListTile(
-            title: Text("Olá"),
-            leading: Icon(Icons.abc),
-          ),
-        ],
+    return ListView(children: [
+      Padding(
+        padding: EdgeInsets.all(20),
+        child: Text("Tap in the word to remove it!"),
       ),
-    );
+      ...favorites
+          .map(
+            (f) => ListTile(
+              onTap: () {
+                appState.removeFavorite(f);
+              },
+              title: Text(f.asUpperCase),
+              leading: Icon(Icons.star),
+            ),
+          )
+          .toList(),
+    ]);
   }
 }
 
