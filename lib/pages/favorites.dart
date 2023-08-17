008a51dc8e7533144d8dd2074ef1d5c5281e32c0
Luan Teixeira <luanlts@outlook.com>
2023-08-17 09:26:42 -0300

    feat(favorites): mensagem para lista vazia

--- lib/pages/favorites.dart
+++ lib/pages/favorites.dart
@@ -7,11 +7,19 @@ class FavoritesPage extends StatelessWidget {
   Widget build(BuildContext context) {
     var appState = context.watch<MyAppState>();
     var favorites = appState.favorites;
+
+    var label = favorites.isEmpty
+        ? Padding(
+            padding: EdgeInsets.all(20),
+            child: Text("You not have favorites words yet!"),
+          )
+        : Padding(
+            padding: EdgeInsets.all(20),
+            child: Text("Tap in the word to remove it!"),
+          );
+
     return ListView(children: [
-      Padding(
-        padding: EdgeInsets.all(20),
-        child: Text("Tap in the word to remove it!"),
-      ),
+      label,
       ...favorites
           .map(
             (f) => ListTile(
